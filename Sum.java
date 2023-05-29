00daa6c16081a6b0bddea67acb83ab242d0593b0
Vaishnavi0210 <vvg29082000@gmail.com>
2023-05-29 12:49:11 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    
+    static int mySum(int x, int y){
+        return x+y;
+    }
+    public static void main(String []args){
+
+        int numSum = mySum(88,11);
+        System.out.println("Sum is : "+numSum);
+
+    }
+}
