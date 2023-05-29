51bd664790ecb6a54f06cf011a07a1724382a1d6
Vaishnavi0210 <vvg29082000@gmail.com>
2023-05-29 13:31:42 +0530

    added multiply

---
+++ Multiply.java
@@ -0,0 +1,15 @@
+public class Multiply {
+
+    static int myMul(int x, int y){
+        return x*y;
+    }
+
+
+    public static void main(String [] args){
+        int myMul = myMul(5,20);
+        System.out.println("multiplication is:" +myMul);
+    }
+
+
+    
+}
